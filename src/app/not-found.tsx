6abd83cfbed3e4a5775b7b7b2b8259ae42f349ba
Lukasz Kowalski <lkowalski9090@gmail.com>
2025-02-03 12:30:39 +0100

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = (): JSX.Element => {
+  return (
+    <main>
+      <h1>Not found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
